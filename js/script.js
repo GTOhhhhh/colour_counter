522ab05d0c09e23940795ac72226457bff116630
Karl Spiegel <karl.spiegel@servian.com>
2019-07-03 12:54:38 +1000

    built frontend

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	 
+		$('.custom-file-input').on('change',function(){
+			console.log('change');
+			$(this).next('.custom-file-label').addClass("selected").html($(this).val());
+		});
+	
+});
