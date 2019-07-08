934600dcc1c5442f21064a90799bca3b89a8da2e
Karl Spiegel <karl.spiegel@servian.com>
2019-07-08 11:23:51 +1000

    algo working

---
+++ _test.py
@@ -0,0 +1,58 @@
+from .services.colour_counter import four_neighbours, flood_fill, colour_counter
+import numpy as np
+
+matrix = np.zeros((5, 3))
+matrix[0, 0] = 255
+matrix[1, 0] = 255
+matrix[0, 1] = 255
+matrix[-1, -1] = 255
+matrix[-1, -2] = 255
+matrix[2, 2] = 100
+shape = [5,3]
+
+
+# [[255. 255.   0.]
+#  [255.   0.   0.]
+#  [  0.   0. 100.]
+#  [  0.   0.   0.]
+#  [  0. 255. 255.]]
+
+def test_four_neighbours():
+    assert sorted(four_neighbours(matrix, shape, 0, 0)) == sorted([[1, 0], [0, 1]])
+    assert sorted(four_neighbours(matrix, shape, 4, 2)) == sorted([[4, 1]])
+    assert sorted(four_neighbours(matrix, shape, 2, 1)) == sorted([[2, 0], [1, 1], [3, 1]])
+
+
+def test_flood_fill():
+    visited = np.zeros((5, 3))
+    visited[2, 2] = 1
+    assert flood_fill(matrix, shape, visited, 2, 2)[2, 2] == visited[2, 2]
+
+    visited[0, 0] = 1
+    visited[1, 0] = 1
+    visited[0, 1] = 1
+    for i, row in enumerate(flood_fill(matrix, shape, visited, 0, 0)):
+        for j, elem in enumerate(row):
+            assert visited[i, j] == elem
+
+    for i, row in enumerate(flood_fill(matrix, shape, visited, 0, 0)):
+        for j, elem in enumerate(row):
+            assert visited[i, j] == elem
+
+    for i, row in enumerate(matrix):
+        for j, elem in enumerate(row):
+            if elem == 0:
+                visited[i, j] = 1
+
+    for i, row in enumerate(flood_fill(matrix, shape, visited, 0, 2)):
+        for j, elem in enumerate(row):
+            assert visited[i, j] == elem
+
+
+def test_colour_counter():
+    output = np.zeros(256)
+    output[0] = 1
+    output[-1] = 2
+    output[100] = 1
+    for i, val in enumerate(output):
+        assert colour_counter(matrix, shape)[i] == val
--- services/colour_counter.py
+++ services/colour_counter.py
@@ -2,24 +2,58 @@
 
 import numpy as np
 import argparse
+from collections import deque
 
 import sys
 
 np.set_printoptions(threshold=sys.maxsize)
 
 
-def colour_counter(input_file, shape):
-    arr = np.fromfile(args.input_file, dtype='uint8', sep='')
-    matrix = arr.reshape([int(i) for i in args.shape.split(',')])
-    print(matrix)
+def colour_counter(matrix, shape):
+    """Finds the number of continuous areas for each greyscale colour in an image
+
+    Parameters:
+        matrix (numpy ndarray): The file location of the spreadsheet
+        shape (list): x, y dimensions of the array
+
+    Returns:
+        visited (numpy array): an array of length 256 representing the number of different sections for each colour
+    """
     output = np.zeros(256)  # 0 is black, 255 is white
-    for row in matrix:
-        for i in row:
-            perimeter_walk(i)
+    visited = np.zeros(shape)
+    for i, row in enumerate(matrix):
+        for j, elem in enumerate(row):
+
+            if visited[i, j]:
+                continue
+            else:
+                visited = flood_fill(matrix, shape, visited, i, j)
+                output[int(matrix[i, j])] += 1
+                visited[i, j] = 1
+    return output
 
 
-def perimeter_walk(start):
-    return start
+def flood_fill(matrix, shape, visited, i, j):
+    """Finds the area of continuous elements with a specific colour value"""
+    stack = deque()
+    stack.append((i, j))
+    while stack:
+        curr = stack.pop()
+        if visited[curr[0], curr[1]]:
+            continue
+        else:
+            visited[curr[0], curr[1]] = 1
+            stack.extend(four_neighbours(matrix, shape, curr[0], curr[1]))
+    return visited
+
+
+def four_neighbours(matrix, shape, i, j):
+    """Returns four adjacent elements with the same colour as the target element"""
+    indexes = [[i - 1, j], [i + 1, j], [i, j + 1], [i, j - 1]]  # north, south, east, west
+    indexes = [idx for idx in indexes if idx[0] > -1 and idx[1] > -1]  # remove negative indexes
+    indexes = [idx for idx in indexes if idx[0] < shape[0] and idx[1] < shape[1]]  # remove out of bounds indexes
+    neighbours = [idx for idx in indexes if matrix[idx[0], idx[1]] == matrix[i, j]]  # remove different colours
+    return neighbours
 
 
 if __name__ == "__main__":
@@ -28,6 +62,7 @@ if __name__ == "__main__":
     parser.add_argument('--shape', required=True,
                         help='specifies the image shape of the provided binary file')
     args = parser.parse_args()
-    colour_counter(args.input_file, args.shape)
-
-    # print(A)
+    shape = [int(i) for i in args.shape.split(',')]
+    arr = np.fromfile(args.input_file, dtype='uint8', sep='')
+    matrix = arr.reshape(shape)
+    colour_counter(matrix, shape)
