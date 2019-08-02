581456868bb6f6c4600c2aad88786a8c8d9b6519
Karl Spiegel <karl.spiegel@servian.com>
2019-08-02 14:15:33 +1000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='colour_counter',
       version='1.0',
       py_modules=['app', 'services.count_areas', 'utils.disjoint_set', 'utils.img_bin_converter', 'utils.union_find'],
-      packages=['flask'
+      packages=['flask',
                 'numpy',
                 'pytest',
                 'opencv-python',
