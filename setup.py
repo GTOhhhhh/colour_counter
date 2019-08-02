58d0644c14ea6e95378de5fc4706da8cf5664395
Karl Spiegel <karl.spiegel@servian.com>
2019-08-02 14:22:57 +1000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(name='colour_counter',
       version='1.0',
       py_modules=['app', 'services.count_areas', 'utils.disjoint_set', 'utils.img_bin_converter', 'utils.union_find'],
-      packages=['flask',
+      install_requires=['flask',
                 'numpy',
                 'pytest',
                 'opencv-python',
                 'flask_socketio',
-                # 'numba',
+                'numba',
                 ],
       )
