cf5846833e514c903da86c9b8e46ffc63dfe5dc9
Karl Spiegel <karl.spiegel@servian.com>
2019-08-02 14:02:06 +1000

    tidied up, fixed app services call

--- services/count_areas.py
+++ services/count_areas.py
@@ -4,57 +4,39 @@ import numpy as np
 import argparse
 from utils.union_find import UnionFind
 from numba import jit, njit, jitclass
-from numbba import
 
+# @njit
+def count_areas(matrix, shape):
+    output = np.zeros(256, dtype='uint8')
+    uf = UnionFind(shape[0] * shape[1])
+    for i in range(shape[0] * shape[1]):
+        elem = matrix[i]
+        west = i - 1 if i % shape[1] != 0 else None
+        west_elem = None if west is None else matrix[west]
+        north = i - shape[1] if i - shape[1] >= 0 else None
+        north_elem = None if north is None else matrix[north]
+        if west_elem != elem and north_elem != elem:
+            continue
 
-spec = [
-    ('value', int32),               # a simple scalar field
-    ('array', float32[:]),          # an array field
-]
+        elif west_elem == elem and north_elem != elem:
+            uf.union(west, i)
 
-@jitclass
-class ColourCounter(object):
-    def __init__(self, matrix, shape):
-        self.matrix = matrix
-        self.shape = shape
-        self.output = np.zeros(256, dtype='uint8')
-        self.uf = UnionFind(shape[0] * shape[1])
+        elif west_elem != elem and north_elem == elem:
+            uf.union(north, i)
 
-    # @jit
-    # def count_areas(self):
-    #     for i in range(shape[0] * shape[1]):
-    #         elem = self.matrix[i]
+        else:  # north and west are both neighbours
+            uf.union(west, north)
+            uf.union(i, west)
 
-    @njit
-    def count_areas(self):
-        for i in range(shape[0]*shape[1]):
-            elem = self.matrix[i]
-            west = i - 1 if i % shape[1] != 0 else None
-            west_elem = None if west is None else self.matrix[west]
-            north = i - shape[1] if i - shape[1] >= 0 else None
-            north_elem = None if north is None else self.matrix[north]
-            if west_elem != elem and north_elem != elem:
-                continue
+    result = set()
+    for i in uf.elems:
+        result.add(uf.find(i))
 
-            elif west_elem == elem and north_elem != elem:
-                self.uf.union(west, i)
+    for i in result:
+        colour = matrix[i]
+        output[colour] += 1
 
-            elif west_elem != elem and north_elem == elem:
-                self.uf.union(north, i)
-
-            else:  # north and west are both neighbours
-                self.uf.union(west, north)
-                self.uf.union(i, west)
-
-        result = set()
-        for i in self.uf.elems:
-            result.add(self.uf.find(i))
-
-        for i in result:
-            colour = self.matrix[i]
-            self.output[colour] += 1
-
-        return self.output
+    return output
 
 
 if __name__ == "__main__":
@@ -65,5 +47,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     shape = [int(i) for i in args.shape.split(',')]
     matrix = np.fromfile(args.input_file, dtype='uint8', sep='')
-    colour_counter = ColourCounter(matrix, shape)
-    print(colour_counter.count_areas())
+    print(count_areas(matrix, shape))
